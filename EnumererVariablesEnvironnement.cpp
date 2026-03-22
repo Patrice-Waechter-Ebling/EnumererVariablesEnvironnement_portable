1f3a17c25b2bcf32591bd52afcee08b46f9e7777
Patrice Wächter-Ebling <patwefr33@hotmail.com>
2026-03-22 00:57:00 -0400

    Remove commented-out code and clean up formatting

--- EnumererVariablesEnvironnement.cpp
+++ EnumererVariablesEnvironnement.cpp
@@ -2,8 +2,6 @@
 //
 
 #include <iostream>
-//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
-
 extern char** environ;
 int main() {
     for (char** env = environ; *env != nullptr; ++env) {
